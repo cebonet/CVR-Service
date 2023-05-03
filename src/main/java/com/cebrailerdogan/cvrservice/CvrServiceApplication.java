6e23d5cb92a69b9135d53e2a06242e07cae6b01b
Cebrail Erdogan <erdogan.cebrail@gmail.com>
2023-05-03 21:33:08 +0200

    Initial commit

---
+++ src/main/java/com/cebrailerdogan/cvrservice/CvrServiceApplication.java
@@ -0,0 +1,13 @@
+package com.cebrailerdogan.cvrservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CvrServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CvrServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cebrailerdogan/cvrservice/CvrServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cebrailerdogan.cvrservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvrServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
