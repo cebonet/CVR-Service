acfabd738fe2e54e20f17fbd31a3851c5c497b9e
Cebrail Erdogan <erdogan.cebrail@gmail.com>
2023-05-08 14:43:57 +0200

    Search query enhanced

--- src/main/java/com/cebrailerdogan/cvrservice/repository/CompanyRepository.java
+++ src/main/java/com/cebrailerdogan/cvrservice/repository/CompanyRepository.java
@@ -3,24 +3,21 @@ package com.cebrailerdogan.cvrservice.repository;
 import com.cebrailerdogan.cvrservice.domain.Company;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 
 import java.util.Optional;
 
 
 public interface CompanyRepository extends JpaRepository<Company, Long> {
 
-
     Optional<Company> findByVat(Long vat);
 
     Optional<Company> findByName(String name);
 
-    @Query(value = "SELECT vat, _protected, c.address, address_co, city, city_name, companycode, companydesc, creditbankrupt, creditstartdate, creditstatus, email, employees, end_date, fax, industry_code, industrydesc, c.name, owners, phone, start_date, t, version, zip_code\n" +
-            "FROM public.company as c " +
-            "LEFT JOIN public.production_unit as p on c.vat = p.vat_id " +
-            "WHERE CAST(c.vat AS character varying) = ?1 " +
-            "OR c.name = ?1 " +
-            "OR CAST(p.pno AS character varying) = ?1",
-            nativeQuery = true)
-    Optional<Company> findByVatOrNameOrProductionUnit(String search);
-
+    @Query("SELECT c from Company c " +
+            "LEFT JOIN c.productionUnits pus " +
+            "WHERE CAST (pus.pno AS string) = :search " +
+            "OR lower(c.name) like concat('%', lower(:search), '%') " +
+            "OR CAST (c.vat AS string) = :search")
+    Optional<Company> findByVatOrNameOrProductionUnit(@Param("search") String search);
 }
